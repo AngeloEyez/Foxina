2dec8ce645f2e0ae5c75d71df9c60fdbbbe62bb2
Gaven <gavenhuang@gmail.com>
2026-05-04 16:51:22 +0800

    update worktree

--- publish.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem =========================================================
-rem Foxina Publish Launcher
-rem =========================================================
-
-powershell -ExecutionPolicy Bypass -NoProfile -File "%~dp0publish.ps1"
